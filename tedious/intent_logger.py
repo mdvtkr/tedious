b1e2607dd17002ee814f75f08eb4b495007b7629
mdvtkr <mdvtkr@gmail.com>
2024-02-15 09:00:37 +0900

    [MOD] intent_logger format, level

--- tedious/intent_logger.py
+++ tedious/intent_logger.py
@@ -1,18 +1,16 @@
-
 import logging
 from rich.logging import RichHandler
+from rich.console import Console
 
-def get(name, format="%(message)s", level="NOTSET"):
+def get(name, format="[%(name)-20s] %(message)s", level=logging.DEBUG):
     logger = logging.getLogger(name)
     formatter = logging.Formatter(format)
-    handler = RichHandler(rich_tracebacks=True)
+    handler = RichHandler(rich_tracebacks=True, console=Console(width=150))
     handler.setFormatter(formatter)
-    logging.basicConfig(
-        level=level, 
-        handlers=[handler]
-    )
+    logger.handlers = [handler]
+    logger.setLevel(level)
 
     info = lambda x, indent=0: logger.info("  "*indent + str(x).replace('\n', '\n'+"  "*indent))
     dbg = lambda x, indent=0: logger.debug("  "*indent + str(x).replace('\n', '\n'+"  "*indent))
     err = lambda x, indent=0: logger.error("  "*indent + str(x).replace('\n', '\n'+"  "*indent))
-    return info, dbg, err
+    return info, dbg, err, logger
